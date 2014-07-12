706474cb7aacd4eaa89c1979026bfb585ce516e1
Mali Plavi <marko.raspica@gmail.com>
2014-07-12 14:29:08 +0200

    Added Messages app, removed Spawn app

--- src/main/java/com/tenjava/entries/Mali_Plavi/t2/listeners/Listener.java
+++ src/main/java/com/tenjava/entries/Mali_Plavi/t2/listeners/Listener.java
@@ -1,25 +1,26 @@
 package com.tenjava.entries.Mali_Plavi.t2.listeners;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.material.MaterialData;
-
-import com.avaje.ebeaninternal.server.deploy.BeanDescriptor.EntityType;
 
 public class Listener implements org.bukkit.event.Listener{
 	public Inventory i;
-	public Inventory spawn;
+	public HashMap<Player,Boolean> to = new HashMap<Player,Boolean>();
+	public HashMap<Player,Boolean> ms = new HashMap<Player,Boolean>(); 
+	public String msg;
+	public Player player;
 	@EventHandler
 	public void onPlayerClick(PlayerInteractEvent e){
 		Player p = e.getPlayer();
@@ -34,61 +35,67 @@ public class Listener implements org.bukkit.event.Listener{
 				//sm.setLore(sl);
 				//s.setItemMeta(sm);
 				//---------------------------------
-				ItemStack s = new ItemStack(Material.MONSTER_EGG,1,(short) 1,(byte)1);
-				ItemMeta sm = s.getItemMeta();
-				ArrayList<String> sl = new ArrayList<String>();
-				sl.add(ChatColor.GRAY + "Spawn Mobs");
-				sm.setDisplayName("Spawn");
-				sm.setLore(sl);
-				s.setItemMeta(sm);
+				ItemStack m = new ItemStack(Material.PAPER);
+				ItemMeta mm = m.getItemMeta();
+				ArrayList<String> ml = new ArrayList<String>();
+				ml.add(ChatColor.GRAY + "Send Messages");
+				mm.setDisplayName("Messages");
+				mm.setLore(ml);
+				m.setItemMeta(mm);
 				
 				
 				//if(p.getItemInHand().getItemMeta().getDisplayName() == "Phone"){
 					i = Bukkit.createInventory(null, 27, "Phone");
-					//i.setItem(1, s);
+					i.setItem(1, m);
 					p.openInventory(i);
 				//}
 			}	
 		}
 	}
 	
-	@SuppressWarnings({"deprecation"})
 	@EventHandler
 	public void onInventoryClick(InventoryClickEvent e){
 		Player p = (Player) e.getWhoClicked();
-		if(e.getCurrentItem().getType() == Material.MONSTER_EGG){
-			if(e.getCurrentItem().getData().getData() == 1){
+		if(e.getCurrentItem().getType() == Material.PAPER){
+			p.sendMessage("pass");
 				if(e.getInventory().getTitle() == "Phone"){
-				ItemStack ci = new ItemStack(Material.MONSTER_EGG,1,(short)1,(byte)50);
-				ItemMeta cm = ci.getItemMeta();
-				cm.setDisplayName("Creeper");
-				ci.setItemMeta(cm);
-				//
-				ItemStack si = new ItemStack(Material.MONSTER_EGG,1,(short)1,(byte)51);
-				ItemMeta sm = ci.getItemMeta();
-				sm.setDisplayName("Skeleton");
-				si.setItemMeta(sm);
+					p.sendMessage("pass");
+					p.closeInventory();
+				p.sendMessage("TO(say the person):");
+				to.put(p, true);
 				
-				
-				spawn = Bukkit.createInventory(null, 54,"Spawn");
-				spawn.setItem(0, ci);
-				spawn.setItem(2, si);
-				p.openInventory(spawn);
-				}
+				//msg.setItem(1, new ItemStack(Material.THIN_GLASS));
 			}
 		}
 	}
 	@EventHandler
-	public void click(InventoryClickEvent e){
-		Player p = (Player) e.getWhoClicked();
-		if(e.getCurrentItem().getType() == Material.MONSTER_EGG){
-			if(e.getCurrentItem().getData().getData() == (byte)50){
-				p.sendMessage("paass");
-				if(e.getInventory().getTitle() == "Spawn"){
-				p.closeInventory();
-				p.getWorld().spawnEntity((Location) p.getTargetBlock(null, 200).getLocation().add(0, 2, 0), org.bukkit.entity.EntityType.CREEPER);
+	public void onChat(AsyncPlayerChatEvent e){
+		Player p = e.getPlayer();
+		String m = e.getMessage();
+		if(to.containsKey(p) | to.get(p).equals(true)){
+			if(to.get(p).equals(true)){
+				if(m.equals(p)){
+						p.sendMessage(ChatColor.DARK_RED + "You can't send message to yourself");
+						p.sendMessage("TO(say the person):");
+						e.setCancelled(true);
+				}
+				for(Player all : Bukkit.getOnlinePlayers()){
+					if(all.equals(m)){
+						player = all;
+						e.setCancelled(true);
+						to.put(p, false);
+						ms.put(p, true);
+						p.sendMessage("Message:");
+						break;
+					}
+					}
+				p.sendMessage(ChatColor.DARK_RED + "That player is not on the server");
 				}
 			}
+		if(ms.containsKey(p) | ms.get(p).equals(true)){
+			msg = m;
+			player.sendMessage(ChatColor.AQUA + "[" + p + " -> You] " + msg);
+			e.setCancelled(true);
+		}
 		}
 	}
-}
